6bee76ca7a525a67117e61e5494772d3d4ff1c63
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-03-29 16:42:44 -0300

    1.1

--- src/main/java/com/apixpress/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/com/apixpress/ecommerce/repository/ProdutoRepository.java
@@ -9,12 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
-    public List<Produto> findAllByValorLessThanEqual (float valor);
-	
-	public List<Produto> findAllByValorGreaterThanEqual (float valor);
+
+    public List<Produto> findAllByValorLessThanEqual(float valor);
+
+    public List<Produto> findAllByValorGreaterThanEqual(float valor);
 
     @Query(value = "select * from tb_produtos inner join tb_lojas on tb_lojas.idLoja = tb_produtos.loja_id_loja where tb_loja.nomeLoja = :nomeLoja", nativeQuery = true)
-	public List<Produto> findAllProdutoByNomeLoja(@Param("nomeLoja") String nomeLoja);
+    public List<Produto> findAllProdutoByNomeLoja(@Param("nomeLoja") String nomeLoja);
 
 }
