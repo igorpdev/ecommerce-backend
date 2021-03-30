a927479cae799c5781ab0886914efc9db5b5ce2f
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-03-30 16:43:37 -0300

    1.5

--- src/main/java/com/apixpress/ecommerce/repository/InscricaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apixpress.ecommerce.repository;
-/*
-import com.apixpress.ecommerce.model.Inscricao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
-    
-} */
